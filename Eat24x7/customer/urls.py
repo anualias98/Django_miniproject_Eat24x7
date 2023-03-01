2714178ae9e88714e883bec1737fcac2f198bfa7
anualias98 <122264818+anualias98@users.noreply.github.com>
2023-03-01 20:49:53 +0530

    first django commit

---
+++ Eat24x7/customer/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import Index, About, Order,signin,signup
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('accounts/', include('allauth.urls')),
+    # path('restaurant/', include('restaurant.urls')),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('order/', Order.as_view(), name='order'),
+    path('signin/',signin,name='signin'),
+    path('signup/',signup,name='signup'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Eat24x7/customer/views.py
@@ -0,0 +1,166 @@
+import json
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+from django.core.mail import send_mail
+from .models import MenuItem, Category, OrderModel
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'about.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(
+            category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        # pass into context
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+
+        # render the template
+        return render(request, 'order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            state=state,
+            zip_code=zip_code
+        )
+        order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to the user
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+#         return redirect('order-confirmation', pk=order.pk)
+#
+#
+# class OrderConfirmation(View):
+#     def get(self, request, pk, *args, **kwargs):
+#         order = OrderModel.objects.get(pk=pk)
+#
+#         context = {
+#             'pk': order.pk,
+#             'items': order.items,
+#             'price': order.price,
+#         }
+
+        return render(request, 'order_confirmation.html', context)
+
+    # def post(self, request, pk, *args, **kwargs):
+    #     data = json.loads(request.body)
+    #
+    #     if data['isPaid']:
+    #         order = OrderModel.objects.get(pk=pk)
+    #         order.is_paid = True
+    #         order.save()
+    #
+    #     return redirect('payment-confirmation')
+
+
+# class OrderPayConfirmation(View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'order_pay_confirmation.html')
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+
+        myuser.save()
+
+        return redirect('signin')
+
+    return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.username
+            return render(request, 'base.html', {'fname': fname})
+
+        else:
+            return redirect('signin')
+
+    return render(request, 'signin.html')
+
+def signout(request):
+    logout(request)
+    messages.success(request, 'Logged out Successfully!')
+    return redirect('signin')
